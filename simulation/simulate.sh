7c259a9330684d93c5eaa82360f39e6fe8637b03
kirsty618 <weikirsty07@gmail.com>
2026-05-21 15:37:34 +0100

    parser2

--- simulation/simulate.sh
+++ simulation/simulate.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
 echo "Compiling..."
-iverilog -o sim ../verilog/test_AXIS.v ../verilog/packer.v ../verilog/pixel_generator.v
+/c/iverilog/bin/iverilog -o sim ../verilog/test_AXIS.v ../verilog/packer.v ../verilog/pixel_generator.v
 
 echo "Simulating..."
-vvp sim
-
-echo "Moving VCD file..."
-mv test.vcd ../simulation/test.vcd
+/c/iverilog/bin/vvp sim
 
 echo "Generating image..."
-cd ../simulation
 python vcd_to_image.py
 
 echo "Done - check simulation/output.png"
